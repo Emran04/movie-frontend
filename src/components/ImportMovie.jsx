9a4495ef527971e54d2dc2d9c69a7cd10965fa55
Emran04 <emran04ch@gmail.com>
2022-10-04 00:42:03 +0600

    update success message

--- src/components/ImportMovie.jsx
+++ src/components/ImportMovie.jsx
@@ -7,11 +7,18 @@ import InputLabel from '@mui/material/InputLabel';
 import MenuItem from '@mui/material/MenuItem';
 import FormControl from '@mui/material/FormControl';
 import Select from '@mui/material/Select';
+import Snackbar from '@mui/material/Snackbar';
+import MuiAlert from '@mui/material/Alert';
 import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
 import { DesktopDatePicker } from '@mui/x-date-pickers/DesktopDatePicker';
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 import dayjs from 'dayjs';
 import { importMovie } from './../services/admin-movie';
+
+const Alert = React.forwardRef(function Alert(props, ref) {
+  return <MuiAlert elevation={6} ref={ref} variant="filled" {...props} />;
+});
+
 
 const ImportMovie = (props) => {
   const { movie } = props
@@ -21,6 +28,16 @@ const ImportMovie = (props) => {
     rent_to: dayjs(),
     plan: 'basic'
   });
+
+  const [open, setOpen] = React.useState(false);
+
+  const handleClose = (event, reason) => {
+    if (reason === 'clickaway') {
+      return;
+    }
+
+    setOpen(false);
+  };
 
   const handleChange = (fieldName, value) => {
     setData({
@@ -40,65 +57,74 @@ const ImportMovie = (props) => {
       plan: data.plan,
     };
     importMovie(values)
-      .then((res) => console.log(res.data))
+      .then((res) => {
+        setOpen(true)
+      })
       .catch((err) => console.log(err))
   }
 
   return (
-    <Box
-      sx={{
-        '& > :not(style)': { m: 1, width: '100%' },
-      }}
-      component="form" onSubmit={handleSubmit}
-      autoComplete="off"
-    >
-      <h3>{movie?.Title}</h3>
-      <p>IMDB ID: {movie?.imdbID}</p>
-      <LocalizationProvider dateAdapter={AdapterDayjs}>
-        <Stack spacing={3}>
-          <DesktopDatePicker
-            label="Rent from"
-            inputFormat="MM/DD/YYYY"
-            value={data.rent_from}
-            onChange={(value) => handleChange('rent_from', value)}
-            renderInput={(params) => <TextField {...params} />}
-          />
-          <DesktopDatePicker
-            label="Rent to"
-            inputFormat="MM/DD/YYYY"
-            value={data.rent_to}
-            onChange={(value) => handleChange('rent_to', value)}
-            renderInput={(params) => <TextField {...params} />}
-          />
-          <FormControl fullWidth>
-            <InputLabel id="select-plan">Plan</InputLabel>
-            <Select
-              labelId="select-plan"
-              label="Plan"
-              name="plan"
-              value={data.plan}
-              onChange={(e) => handleChange('plan', e.target.value)}
-            >
-              <MenuItem value={`basic`}>Basic</MenuItem>
-              <MenuItem value={`premium`}>Premium</MenuItem>
-            </Select>
-          </FormControl>
-          <TextField
-            margin="normal"
-            required
-            fullWidth
-            id="price"
-            label="Rent price"
-            name="price"
-            autoComplete="price"
-          />
-        </Stack>
-      </LocalizationProvider>
-      <Button variant="contained" component="label">
-        Submit
-        <input type="submit" hidden />
-      </Button>
-    </Box>
+    <div>
+      <Snackbar open={open} autoHideDuration={6000} onClose={handleClose}>
+        <Alert onClose={handleClose} severity="success" sx={{ width: '100%' }}>
+          Successfully Imported!
+        </Alert>
+      </Snackbar>
+      <Box
+        sx={{
+          '& > :not(style)': { m: 1, width: '100%' },
+        }}
+        component="form" onSubmit={handleSubmit}
+        autoComplete="off"
+      >
+        <h3>{movie?.Title}</h3>
+        <p>IMDB ID: {movie?.imdbID}</p>
+        <LocalizationProvider dateAdapter={AdapterDayjs}>
+          <Stack spacing={3}>
+            <DesktopDatePicker
+              label="Rent from"
+              inputFormat="MM/DD/YYYY"
+              value={data.rent_from}
+              onChange={(value) => handleChange('rent_from', value)}
+              renderInput={(params) => <TextField {...params} />}
+            />
+            <DesktopDatePicker
+              label="Rent to"
+              inputFormat="MM/DD/YYYY"
+              value={data.rent_to}
+              onChange={(value) => handleChange('rent_to', value)}
+              renderInput={(params) => <TextField {...params} />}
+            />
+            <FormControl fullWidth>
+              <InputLabel id="select-plan">Plan</InputLabel>
+              <Select
+                labelId="select-plan"
+                label="Plan"
+                name="plan"
+                value={data.plan}
+                onChange={(e) => handleChange('plan', e.target.value)}
+              >
+                <MenuItem value={`basic`}>Basic</MenuItem>
+                <MenuItem value={`premium`}>Premium</MenuItem>
+              </Select>
+            </FormControl>
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              id="price"
+              label="Rent price"
+              name="price"
+              autoComplete="price"
+            />
+          </Stack>
+        </LocalizationProvider>
+        <Button variant="contained" component="label">
+          Submit
+          <input type="submit" hidden />
+        </Button>
+      </Box>
+    </div>
   )
 }
 
--- src/components/Subscribe.jsx
+++ src/components/Subscribe.jsx
@@ -3,12 +3,28 @@ import Box from '@mui/material/Box';
 import TextField from '@mui/material/TextField';
 import Button from '@mui/material/Button';
 import Stack from '@mui/material/Stack';
+import Snackbar from '@mui/material/Snackbar';
+import MuiAlert from '@mui/material/Alert';
 import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 import { rentMovie } from '../services/movies';
 
+const Alert = React.forwardRef(function Alert(props, ref) {
+  return <MuiAlert elevation={6} ref={ref} variant="filled" {...props} />;
+});
+
+
 const Subscribe = (props) => {
   const { movieId } = props
+  const [open, setOpen] = React.useState(false);
+
+  const handleClose = (event, reason) => {
+    if (reason === 'clickaway') {
+      return;
+    }
+
+    setOpen(false);
+  };
 
   const handleSubmit = (e) => {
     e.preventDefault()
@@ -19,46 +35,55 @@ const Subscribe = (props) => {
       payment: formData.get('payment'),
     };
     rentMovie(values)
-      .then((res) => console.log(res.data))
+      .then((res) => {
+        setOpen(true)
+      })
       .catch((err) => console.log(err))
   }
 
   return (
-    <Box
-      sx={{
-        '& > :not(style)': { m: 1, width: '100%' },
-      }}
-      component="form" onSubmit={handleSubmit}
-      autoComplete="off"
-    >
-      <p>Movie ID: {movieId}</p>
-      <LocalizationProvider dateAdapter={AdapterDayjs}>
-        <Stack spacing={3}>
-          <TextField
-            margin="normal"
-            required
-            fullWidth
-            id="price"
-            label="Payment amount"
-            name="payment"
-            autoComplete="price"
-          />
-          <TextField
-            margin="normal"
-            required
-            fullWidth
-            id="days"
-            label="Input days for rent"
-            name="days"
-            autoComplete="days"
-          />
-        </Stack>
-      </LocalizationProvider>
-      <Button variant="contained" component="label">
-        Submit
-        <input type="submit" hidden />
-      </Button>
-    </Box>
+    <div>
+      <Snackbar open={open} autoHideDuration={6000} onClose={handleClose}>
+        <Alert onClose={handleClose} severity="success" sx={{ width: '100%' }}>
+          Successfully subscribed!
+        </Alert>
+      </Snackbar>
+      <Box
+        sx={{
+          '& > :not(style)': { m: 1, width: '100%' },
+        }}
+        component="form" onSubmit={handleSubmit}
+        autoComplete="off"
+      >
+        <p>Movie ID: {movieId}</p>
+        <LocalizationProvider dateAdapter={AdapterDayjs}>
+          <Stack spacing={3}>
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              id="price"
+              label="Payment amount"
+              name="payment"
+              autoComplete="price"
+            />
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              id="days"
+              label="Input days for rent"
+              name="days"
+              autoComplete="days"
+            />
+          </Stack>
+        </LocalizationProvider>
+        <Button variant="contained" component="label">
+          Submit
+          <input type="submit" hidden />
+        </Button>
+      </Box>
+    </div>
   )
 }
 
